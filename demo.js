f2a6a771f4d26e20d7bd582486d1dda752f8ede8
Tim Perry <pimterry@gmail.com>
2016-07-14 22:33:48 +0200

    Do an initial render test on startup

--- demo.js
+++ demo.js
@@ -4,15 +4,21 @@ require("./index.js");
 var express = require("express");
 var app = express();
 
-app.get('/', (req, res) => {
-    components.renderPage(`
-        <html>
-        <head></head>
-        <body>
-            <leaflet-map lat="41.3947688" long="2.0787283" zoom="12"></leaflet-map>
-        </body>
-        </html>`
-    ).then((html) => res.send(html));
+var html = `
+<html>
+<head></head>
+<body>
+    <leaflet-map lat="41.3851" long="2.1734" zoom="12"></leaflet-map>
+</body>
+</html>
+`;
+
+components.renderPage(html).then(() => {
+    console.log("Initial render successful");
+}).catch((err) => {
+    console.error("Error on initial render", err);
 });
+
+app.get('/', (req, res) => components.renderPage(html).then((output) => res.send(output)));
 app.use(express.static('.'));
 app.listen(3000, () => console.log("Server listening on 3000"));

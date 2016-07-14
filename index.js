863f8bdfb6811c53c70ea253507a5c05d22f87a2
Tim Perry <pimterry@gmail.com>
2016-07-14 21:30:47 +0200

    Working server-rendered leaflet

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 var components = require("server-components");
 
+var SandboxedModule = require('sandboxed-module');
+var leaflet = SandboxedModule.require('leaflet', {
+  sourceTransformers: {
+      wrapToInjectGlobals: function (source) {
+        return `
+        module.exports = function (window, document) {
+          var navigator = window.navigator;
+
+          ${source}
+
+          return window.L.noConflict();
+        }`;
+      }
+  }
+});
+
 var LeafletMapElement = components.newElement();
-LeafletMapElement.createdCallback = function () {
-    this.innerHTML = "HI THERE";
+LeafletMapElement.createdCallback = function (document) {
+  var L = leaflet(new components.dom.Window(), document);
+
+  this.clientWidth = 500;
+  this.clientHeight = 500;
+
+  var myMap = L.map(this).setView([51.505, -0.09], 13);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(myMap);
 };
 
 components.registerElement("leaflet-map", { prototype: LeafletMapElement });
